c5f7b04e492e8591b9738ab2b7356395d7cb0d50
xbignell <xavierbignell@gmail.com>
2022-05-18 13:52:20 +0200

    1 file modified

--- src/app/second-example-solution/second-example-solution.component.ts
+++ src/app/second-example-solution/second-example-solution.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectorRef, Component } from '@angular/core';
-import { tap } from 'rxjs';
+import { delay, tap } from 'rxjs';
 import { CarService } from '../car.service';
 
 @Component({
@@ -9,11 +9,6 @@ import { CarService } from '../car.service';
   providers: [CarService],
 })
 export class SecondExampleSolutionComponent {
-  public model$ = this.carService.carModel$.pipe(
-    tap(() => this.changeDetector.detectChanges())
-  );
-  constructor(
-    private carService: CarService,
-    private changeDetector: ChangeDetectorRef
-  ) {}
+  public model$ = this.carService.carModel$.pipe(delay(0));
+  constructor(private carService: CarService) {}
 }
